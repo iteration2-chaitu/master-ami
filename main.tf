0780de6a7e43da82b0ddd9a140d3d275b8fd3447
chaitu <chaitus.ch09@gmail.com>
2024-11-08 13:49:48 -0800

    AMI | install ansible

--- main.tf
+++ main.tf
@@ -8,3 +8,28 @@ resource "aws_instance" "ami" {
 
   }
 }
+
+resource "null_resource" "ansible" {
+
+  connection {
+    type     = "ssh"
+    //user     = var.ssh_user   #"ec2-user"
+    user     = jsondecode(data.vault_generic_secret.ssh.data_json).ansible_user
+    //password = var.ssh_password   #"DevOps321"
+    password = jsondecode(data.vault_generic_secret.ssh.data_json).ansible_password
+    #      host     = aws_instance.instance.public_ip
+    host     = aws_instance.ami.private_ip   #this is once we create nat gateways
+  }
+
+
+  provisioner "remote-exec" {
+
+    inline = [
+
+      "sudo pip3.11 install ansible hvac"
+
+
+    ]
+  }
+
+}
